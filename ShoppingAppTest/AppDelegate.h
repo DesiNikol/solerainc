40398c7b9acfd68414bbb6a6b13709eb452e8ac6
Desi Nikolova <desinikolova@desis-macbook-pro-2.local>
2016-04-01 19:54:42 +0100

    Initial Commit

---
+++ ShoppingAppTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShoppingAppTest
+//
+//  Created by Desi Nikolova on 4/1/16.
+//  Copyright © 2016 Desi Nikolova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShoppingAppTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShoppingAppTest
+//
+//  Created by Desi Nikolova on 4/1/16.
+//  Copyright © 2016 Desi Nikolova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
